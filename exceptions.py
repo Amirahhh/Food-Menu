c516fda044167621662d47aa56ec4bb4abffee3d
Amirahhh <102274422+Amirahhh@users.noreply.github.com>
2022-09-17 19:16:49 -1000

    Update exceptions.py

--- exceptions.py
+++ exceptions.py
@@ -5,3 +5,23 @@ try:
     ans = divide_by(40, 0)
 except Exception as e: 
     print("Something went wrong", e)
+
+    
+    
+def divide_by(a, b):
+    try:
+        return a / b
+    except ZeroDivisionError:
+        return 0
+    except Exception as e:
+        print(e, 'Something went wrong!')
+
+ans = divide_by(10,0)
+print(ans)
+
+
+try:
+    with open('file_does_not_exist.txt', 'r') as file:
+        print(file.read())
+except:
+    print("Unable to locate file")  
